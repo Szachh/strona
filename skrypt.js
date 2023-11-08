6fb4fdcc20aa3680d88155b5dd55fec46dabd71e
Szachh <133863953+Szachh@users.noreply.github.com>
2023-11-08 14:46:39 +0100

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -2,3 +2,22 @@ function powitanie(){
   var name=document.getElementById("name");
   alert("Witaj " + name);
 }
+
+function zamiana(liczba){
+  if(liczba==0){
+    var lista=[0];
+  }
+  else{
+    var lista=[];
+    while(liczba>0){
+      lista.push(liczba%2);
+      liczba=(liczba/2)>>0;
+    }
+  }
+  return lista.reverse().join("");
+}
+function program(){
+  var liczba10=document.getElementById("liczba").value;
+  var liczba2=zamiana(liczba10);
+  document.querySelector("span").innerHTML=liczba2;
+}
